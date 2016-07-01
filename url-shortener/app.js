7053c4a216de012e98e9f8ac86b0d207f0f29d00
dearzui <673008724@163.com>
2016-07-01 18:30:44 +0800

    add route

--- url-shortener/app.js
+++ url-shortener/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config');
+var base58 = require('./base58.js');
+
+var Url = require('./models/url');
+
+mongoose.connect('mongod://' + config.db.host + '/' + config.db.name);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +23,16 @@ app.get('/', function(req, res) {
 
 app.post('/api/shorten', function(req, res) {
 	// shorten url
+	var longUrl = req.body.url;
+	var shortUrl = '';
+
+	Url.findOne()({long_url: longUrl}, function(err, doc) {
+		if (doc) {
+
+		} else {
+
+		}
+	});
 });
 
 app.get(':encoded_id', function(req, res){

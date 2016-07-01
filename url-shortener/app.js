13dea8c24e2af83ea7041b8ac6acac8ae7d8c631
dearzui <673008724@163.com>
2016-07-01 16:14:27 +0800

    update

--- url-shortener/app.js
+++ url-shortener/app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // server files from public folder
 app.use(express.static(path.join(__dirname, 'public')))
--- url-shortener/models/url.js
+++ url-shortener/models/url.js
@@ -4,6 +4,27 @@ var Schema = mongoose.Schema;
 var CounterSchema = Schema({
 	_id: {type: string, required: true},
 	seq: {type: Number, default: 0}
-})
+});
 
 var counter = mongoose.model('counter', CounterSchema);
+
+var urlSchema = new Schema({
+	_id: {type: Number, index: true},
+	created_at: Date
+});
+
+urlSchema.pre('save', function(next) {
+	var doc = this;
+	// find url_count and add 1
+	counter.findeByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1}}, function(error, counter) {
+		if (err)
+			return next(err);
+		doc._id = counter.seq;
+		doc.created_at = new Date();
+		next();
+	});
+});
+
+var Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;

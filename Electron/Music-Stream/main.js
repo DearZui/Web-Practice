b7126533c19cd97a7305bb645bd7043b594d03a7
dearzui <loebmraz@gmail.com>
2016-08-01 12:49:37 +0800

    update

--- Electron/Music-Stream/main.js
+++ Electron/Music-Stream/main.js
@@ -5,22 +5,22 @@ let win;
 function createWindow() {
 	win = new BrowserWindow({width: 1000, height: 600});
 	win.loadURL(`file://${__dirname}/index.html`);
-
-	win.on('closed', () => {
+	//win.webContents.openDevTools();
+	win.on('closed', ()=> {
 		win = null;
 	})
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-	if (process.platform !== darwin) {
+app.on('window-all-closed', ()=> {
+	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 })
 
 app.on('active', () => {
 	if (win === null) {
-		createWindow()
+		createWindow();
 	}
 })
---
+++ Electron/Music-Stream/src/components/ProgressSoundPlayer.js
@@ -0,0 +1,3 @@
+import React {Component, PropTypes} from 'react';
+import { SoundPlayerContainer } from 'react-soundplayer/addons';
+import Track from './Track';

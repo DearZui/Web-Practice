0f796d26246c6a14ec1c2b83aba731a2c4101ece
DearZui <673008724@163.com>
2016-08-14 17:07:53 +0800

    insertion sort

---
+++ JS/Sort/sort-es6.js
@@ -0,0 +1,11 @@
+//insertion sort
+function insertion(input) {
+	for (let i = 1, len = input.length; i < len; i++) {
+		for (let j = i; j > 0; j--) {
+			if (input[j] < input[j-1]) {
+				input[j] = [input[j-1], input[j-1] = input[j]][0];
+			}
+		}
+	}
+	return input;
+}

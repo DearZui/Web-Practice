c81c6cd99fac302a2e0231530b2591bd8c8e7342
DearZui <673008724@163.com>
2016-09-29 22:34:06 +0800

    use a metehod to submit

--- Meteor/meteor/Discover/microscope/lib/collections/posts.js
+++ Meteor/meteor/Discover/microscope/lib/collections/posts.js
@@ -1,8 +1,21 @@
 //We want Posts to existed both in cilent and server, `var` limits the scope in the folder, so we abandon the `var`
 Posts = new Mongo.Collection('posts');
 
-Posts.allow({
-	insert: function(userId, doc) {
-		return !! userId;
+Meteor.methods({
+	postInsert: function(postAttributes) {
+		check(postAttributes, {
+			title: String,
+			url: String
+		});
+		var user = Meteor.user();
+		var post = _.extend(postAttributes, {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date()
+		});
+		var postId = Posts.insert(post);
+		return {
+			_id: postId
+		};
 	}
-})
+});

af4a46c9b1ea5153269de727045bdda38f5db21b
DearZui <673008724@163.com>
2016-11-13 22:11:03 +0800

    template added

--- bookmarking-app/app/App.vue
+++ bookmarking-app/app/App.vue
@@ -1,5 +1,16 @@
 <template>
-	
+	<div id="app">
+	<sidebar
+		:categories="categories"
+		v-on:category-selected="setSelectedCategory">
+		
+	</sidebar>
+	<bookmark-list
+		:bookmarks="bookmarks | filterByCategory selectedCategory"
+		:categories:"categories">
+		
+	</bookmark-list>
+	</div>
 </template>
 
 <script>

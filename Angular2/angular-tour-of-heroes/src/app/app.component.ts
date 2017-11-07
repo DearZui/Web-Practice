9c2d93f1451cffc3b866193df10881b74b42728d
dearzui <673008724@163.com>
2017-11-07 19:52:17 +0800

    editor

--- Angular2/angular-tour-of-heroes/src/app/app.component.ts
+++ Angular2/angular-tour-of-heroes/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
 }

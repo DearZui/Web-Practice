bc997cd751c6560e56d94157d369b2072ff281f6
dearzui <673008724@163.com>
2018-12-14 10:11:01 +0800

    promise startup end

---
+++ Promise/promise-than-passing-value.js
@@ -0,0 +1,18 @@
+function doubleUp(value) {
+    return value*2;
+}
+function increment(value) {
+    return value+1;
+}
+function output(value) {
+    console.log(value);
+}
+
+const promise = Promise.resolve(1);
+promise
+    .then(increment)
+    .then(doubleUp)
+    .then(output)
+    .catch(function(error) {
+        console.error(error);
+    });
